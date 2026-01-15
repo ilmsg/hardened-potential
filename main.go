86a22e86c727ccf60ae9302c4fdf53cc0381bd8d
Eak Netpanya <eak.netpanya@gmail.com>
2026-01-16 01:28:53 +0700

    add pick random emoji

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/ilmsg/hardened-potential/helper"
 	"github.com/ilmsg/hardened-potential/slug"
 )
 
 func main() {
 	newSlug := slug.GenerateSlug()
 	fmt.Printf("Generate Slug: %s\n", newSlug)
+
+	pickEmoji := helper.PickRandomEmoji()
+	fmt.Printf("Pick Random Emoji: %s\n", pickEmoji)
 }
