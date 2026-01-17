da547a27239bf385e65595c3bb568caa33b58163
Eak Netpanya <eak.netpanya@gmail.com>
2026-01-17 22:13:35 +0700

    update api

--- api/article.go
+++ api/article.go
@@ -1,10 +1,53 @@
 package api
 
-type Article struct{}
+import (
+	"github.com/ilmsg/hardened-potential/model"
+	"github.com/ilmsg/hardened-potential/slug"
+	"gorm.io/gorm"
+)
 
-func (article *Article) FindAllArticle() {}
-func (article *Article) FindArticle()    {}
+type IArticleHandler interface {
+	NewArticle(title string) (*model.Article, error)
+	FindAllArticle() (*[]model.Article, error)
+	FindArticle(articleId string) (*model.Article, error)
+}
 
-func NewArticle() *Article {
-	return &Article{}
+type ArticleHandler struct {
+	db *gorm.DB
+}
+
+func (h *ArticleHandler) NewArticle(title string) (*model.Article, error) {
+	newArticle := model.Article{
+		ID:    slug.GenerateSlug(),
+		Title: title,
+	}
+	if tx := h.db.Create(&newArticle); tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	var article model.Article
+	if tx := h.db.First(&article, "id = ?", newArticle.ID); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &article, nil
+}
+
+func (h *ArticleHandler) FindAllArticle() (*[]model.Article, error) {
+	var articles []model.Article
+	if tx := h.db.Find(&articles); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &articles, nil
+}
+
+func (h *ArticleHandler) FindArticle(articleId string) (*model.Article, error) {
+	var article model.Article
+	if tx := h.db.First(&article, "id = ?", articleId); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &article, nil
+}
+
+func NewArticleHandler(db *gorm.DB) IArticleHandler {
+	return &ArticleHandler{db}
 }
--- api/book.go
+++ api/book.go
@@ -1,11 +1,44 @@
 package api
 
-type Book struct{}
+import (
+	"github.com/ilmsg/hardened-potential/model"
+	"gorm.io/gorm"
+)
 
-func (book *Book) FindAllBook() {}
-func (book *Book) FindBook()    {}
-func (book *Book) FindChapter() {}
+type IBookHandler interface {
+	FindAllBook() (*[]model.Book, error)
+	FindBook(bookId string) (*model.Book, error)
+	FindChapter(chapterId string) (*model.Chapter, error)
+}
 
-func NewBook() *Book {
-	return &Book{}
+type BookHandler struct {
+	db *gorm.DB
+}
+
+func (h *BookHandler) FindAllBook() (*[]model.Book, error) {
+	var books []model.Book
+	if tx := h.db.Find(&books); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &books, nil
+}
+
+func (h *BookHandler) FindBook(bookId string) (*model.Book, error) {
+	var book model.Book
+	if tx := h.db.First(&book, "id = ?", bookId); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &book, nil
+}
+
+func (h *BookHandler) FindChapter(chapterId string) (*model.Chapter, error) {
+	var chapter model.Chapter
+	if tx := h.db.First(&chapter, "id = ?", chapterId); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &chapter, nil
+}
+
+func NewBookHandler(db *gorm.DB) IBookHandler {
+	return &BookHandler{db}
 }
