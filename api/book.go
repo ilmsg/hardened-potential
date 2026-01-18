07c98cd2e4a590755a30b8ad520d61f572353029
Eak Netpanya <eak.netpanya@gmail.com>
2026-01-18 18:11:21 +0700

    update api

--- api/book.go
+++ api/book.go
@@ -1,12 +1,17 @@
 package api
 
 import (
+	"fmt"
+
+	"github.com/ilmsg/hardened-potential/helper"
 	"github.com/ilmsg/hardened-potential/model"
+	"github.com/ilmsg/hardened-potential/slug"
 	"gorm.io/gorm"
 )
 
 type IBookHandler interface {
-	FindAllBook() (*[]model.Book, error)
+	NewBook(title string, userId string) (*model.Book, error)
+	FindAllBook() ([]model.Book, error)
 	FindBook(bookId string) (*model.Book, error)
 	FindChapter(chapterId string) (*model.Chapter, error)
 }
@@ -15,12 +20,29 @@ type BookHandler struct {
 	db *gorm.DB
 }
 
-func (h *BookHandler) FindAllBook() (*[]model.Book, error) {
+func (h *BookHandler) NewBook(title string, userId string) (*model.Book, error) {
+	newBook := model.Book{
+		ID:     slug.GenerateSlug(),
+		Title:  fmt.Sprintf("%s %s", helper.PickRandomEmoji(), title),
+		UserId: userId,
+	}
+	if tx := h.db.Create(&newBook); tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	var book model.Book
+	if tx := h.db.First(&book, "id = ?", newBook.ID); tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &book, nil
+}
+
+func (h *BookHandler) FindAllBook() ([]model.Book, error) {
 	var books []model.Book
 	if tx := h.db.Find(&books); tx.Error != nil {
 		return nil, tx.Error
 	}
-	return &books, nil
+	return books, nil
 }
 
 func (h *BookHandler) FindBook(bookId string) (*model.Book, error) {
