d7ee61e9843e70733097c1850de897d17bdef584
Eak Netpanya <eak.netpanya@gmail.com>
2026-01-18 18:10:29 +0700

    update model

--- model/article.go
+++ model/article.go
@@ -1,6 +1,7 @@
 package model
 
 type Article struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	UserId string `json:"user_id"`
 }
--- model/book.go
+++ model/book.go
@@ -1,12 +1,13 @@
 package model
 
 type Book struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	UserId string `json:"user_id"`
 }
 
 type Chapter struct {
 	ID     string `json:"id"`
 	Title  string `json:"title"`
-	BookId uint   `json:"book_id"`
+	BookId string `json:"book_id"`
 }
